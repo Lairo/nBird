0217f2c12ef08ac70d64b93a1e747942055ba355
chajus1 <chajus1@googlemail.com>
2023-06-24 05:07:36 +0100

    pigeon 1-2 eggs max puzzle included

--- nBird/Pigeon.cs
+++ nBird/Pigeon.cs
@@ -1,18 +1,40 @@
-﻿namespace nBird
+﻿using System;
+
+namespace nBird
 {
     class Pigeon : Bird
     {
+        const int MAXIMUMEGGS = 2;
         public override Egg[] LayEggs(int numberOfEggs)
         {
-            Egg[] eggs = new Egg[numberOfEggs];
-            for (int i = 0; i < numberOfEggs; i++)
-            {
-                if (Bird.Randomizer.Next(4) == 0)
-                    eggs[i] = new BrokenEgg("white");
-                else
-                    eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            if (numberOfEggs > MAXIMUMEGGS)
+            {   numberOfEggs = MAXIMUMEGGS;
+                Console.WriteLine("Pigeons can only lay between 1-2eggs at a time");
             }
-            return eggs;
+            //{
+            //    Egg[] eggs = new Egg[2];
+            //    for (int i = 0; i < 2; i++)
+            //    {
+            //        if (Bird.Randomizer.Next(4) == 0)
+            //            eggs[i] = new BrokenEgg("white");
+            //        else
+            //            eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            //    }
+            //    Console.WriteLine("Pigeons can only lay between 1-2eggs at a time");
+            //    return eggs;
+            //}
+            //else
+            
+                Egg[] eggs = new Egg[numberOfEggs];
+                for (int i = 0; i < numberOfEggs; i++)
+                {
+                    if (Bird.Randomizer.Next(4) == 0)
+                        eggs[i] = new BrokenEgg("white");
+                    else
+                        eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+                }
+                return eggs;
+            
         }
     }
 }
